3b122fb67cff1dd7abba1cb80eb9a65b2ed47546
Shubham-Patil <shubhampatil@outlook.com>
2025-01-24 14:50:57 +0530

    Added backend service URL

--- src/app/services/worker.service.ts
+++ src/app/services/worker.service.ts
@@ -9,7 +9,7 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://localhost:8080/api/v1/workers";
+  private getUrl: string = "http://a2cc368d597e74488ad05ea0f02a569a-1241641132.us-east-2.elb.amazonaws.com:8080/api/v1/workers";
 
   constructor(private _httpClient: HttpClient) { }
 
